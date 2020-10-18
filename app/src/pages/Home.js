8ce0d4985eab51322e07bb349a495f28f25d6219
nathannlu <nathanlu_@hotmail.com>
2020-10-18 12:53:34 -0700

    Changed ts to js

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,26 +1,26 @@
-import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import './Home.css';
 
-const Tab2: React.FC = () => {
+const Home = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab2;
+export default Home;
