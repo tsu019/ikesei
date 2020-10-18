91258fe9d82e0e36df3d8094dceeb80610aeddfc
nathannlu <nathanlu_@hotmail.com>
2020-10-18 13:04:30 -0700

    Fixed ExploreContainer import to use JS

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import ExploreContainer from '../components/ExploreContainer.js';
 import './Home.css';
 
 const Home = () => {
