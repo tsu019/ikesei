be5d82713dd3e7ae221251e15f72c8588b6d1ad0
CanFISHfly1 <nathanlu_@hotmail.com>
2020-11-01 23:31:36 -0800

    Set up facebook auth

--- server/server.js
+++ server/server.js
@@ -1,22 +1,46 @@
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const User = require('./models/userModel');
 const graphQLSchema = require('./graphql/schema');
 const graphQLResolvers = require('./graphql/resolvers');
+const authFacebookRoutes = require('./api');
+require('./api/passport')(passport);
 
 // Enviornment variables
 require('dotenv').config();
 
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });	
+});
+
 const app = express();
+
+app.use(session({
+	secret: 'nathan',	
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(
 	cors(),	
 	bodyParser.urlencoded({extended: false}),
 	bodyParser.json()
 );
+
+app.use('/auth/facebook', authFacebookRoutes);
 
 app.use(
 	'/graphql',
